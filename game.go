b152c8d9bc29c7ada384d2c12730bab9fc47b1e8
Josef Baltz <josef@baltz.net>
2019-06-24 17:01:24 -0500

    You can Adopt a Wumpus!

--- game.go
+++ game.go
@@ -23,20 +23,21 @@ func game(session *discordgo.Session, event *discordgo.MessageCreate) {
 	messageContent := strings.Split(strings.ToLower(event.Content), " ")
 	if messageContent[0] == CommandPrefix+"adopt" {
 		if len(messageContent) > 1 {
-		NewWumpus := Wumpus{
-			Credits:   0,
-			Name:      strings.TrimPrefix(event.Content, CommandPrefix+"adopt "),
-			Color:     0,
-			Age:       0,
-			Health:    10,
-			Hunger:    10,
-			Energy:    10,
-			Happiness: 10,
-			Sick:      false,
+			NewWumpus := Wumpus{
+				Credits:   0,
+				Name:      strings.TrimPrefix(event.Content, CommandPrefix+"adopt "),
+				Color:     0,
+				Age:       0,
+				Health:    10,
+				Hunger:    10,
+				Energy:    10,
+				Happiness: 10,
+				Sick:      false,
+			}
+			AddWumpus(event.Author.ID, NewWumpus)
+			session.ChannelMessageSend(event.ChannelID, "Congrats, you have adopted "+NewWumpus.Name+" as your Wumpus!")
+		} else {
+			session.ChannelMessageSend(event.ChannelID, "Your Wumpus needs a name to be adopted!")
 		}
-		AddWumpus(event.Author.ID, NewWumpus)
-		session.ChannelMessageSend(event.ChannelID, "Congrats, you have adopted "+NewWumpus.Name+" as your Wumpus!")
-	} else {
-		session.ChannelMessageSend(event.ChannelID, "Your Wumpus needs a name to be adopted!")
 	}
 }

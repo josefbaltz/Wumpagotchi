4d48e2129d98e56e69b07fd884791c80ce2fbe21
Ryan Jokuti <r.jokuti@gmail.com>
2019-06-24 16:41:35 -0500

    Made an adoption function :D

--- game.go
+++ game.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"strings"
+
+	"github.com/bwmarrin/discordgo"
+)
+
 type Wumpus struct {
 	Credits   int
 	Name      string
@@ -12,6 +18,21 @@ type Wumpus struct {
 	Sick      bool
 }
 
-func game() {
-
+func game(session *discordgo.Session, event *discordgo.MessageCreate) {
+	messageContent := strings.Split(strings.ToLower(event.Content), " ")
+	if messageContent[0] == CommandPrefix+"adopt" {
+		NewWumpus := Wumpus{
+			Credits:   0,
+			Name:      strings.TrimPrefix(event.Content, CommandPrefix+"adopt "),
+			Color:     0,
+			Age:       0,
+			Health:    10,
+			Hunger:    10,
+			Energy:    10,
+			Happiness: 10,
+			Sick:      false,
+		}
+		AddWumpus(event.Author.ID, NewWumpus)
+		session.ChannelMessageSend(event.ChannelID, "Congrats, you have adopted "+NewWumpus.Name+" as your Wumpus!")
+	}
 }

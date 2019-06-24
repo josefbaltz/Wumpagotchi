653bbb1d4a6b20c618b89182e51d1a487c227a76
Josef Baltz <josef@baltz.net>
2019-06-24 15:10:27 -0500

    gcp

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
 
+	"cloud.google.com/go/datastore"
 	"github.com/bwmarrin/discordgo"
+	"google.golang.org/api/option"
 )
 
 func init() {
@@ -24,6 +27,14 @@ func main() {
 		fmt.Println("==Wumpagotchi Error==\nYour start command should be as follows:\nWumpagotchi -t <Discord API Token>")
 		os.Exit(0)
 	}
+
+	ctx = context.Background()
+	gcp, gcpErr := datastore.NewClient(ctx, "wumpagotchi", option.WithCredentialsFile("./WumpagotchiCredentials.json"))
+	if gcpErr != nil {
+		fmt.Println("==Datastore Error==\nFailed to create GCP Client\n" + gcpErr.Error())
+		os.Exit(1)
+	}
+
 	wump, err := discordgo.New("Bot " + DiscordToken)
 	if err != nil {
 		fmt.Println("==Wumpagotchi Error==\n" + err.Error())

50e50ba1112581098255c12d4a15f7537cc62ca7
Josef Baltz <josef@baltz.net>
2019-06-24 14:44:42 -0500

    help command ready for embed

--- basicCommands.go
+++ basicCommands.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"strings"
+
 	"github.com/bwmarrin/discordgo"
 )
 
 func basicCommands(session *discordgo.Session, event *discordgo.MessageCreate) {
-
+	messageContent := strings.Split(strings.ToLower(event.Content), " ")
+	if messageContent[0] == CommandPrefix+"help" {
+		//TODO: Help Embed Here
+		return
+	}
 }

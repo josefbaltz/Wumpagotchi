175b8ae6f2bc68e4034c07c994e250b4d4ee89ca
Ryan Jokuti <r.jokuti@gmail.com>
2019-06-24 14:57:48 -0500

    Created game file

---
+++ game.go
@@ -0,0 +1,35 @@
+package main
+
+type Wumpus struct {
+	Name       string
+	Color      int
+	Age        int
+	Evolution  int
+	Weight     int
+	Health     int
+	State      string
+	Hunger     int
+	Energy     int
+	Discipline int
+	Sickness   bool
+}
+
+func game() {
+
+}
+
+func feed() {
+
+}
+
+func sleep() {
+
+}
+
+func discipline() {
+
+}
+
+func play() {
+
+}

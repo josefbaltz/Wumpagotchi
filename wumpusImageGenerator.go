dda310e562fe36eae8c2022b115865d266a2f3ac
Josef Baltz <josef@baltz.net>
2019-06-25 18:01:48 -0500

    Progress

--- wumpusImageGenerator.go
+++ wumpusImageGenerator.go
@@ -3,7 +3,11 @@ package main
 import (
 	"fmt"
 	"image"
+	"image/color"
 	"net/http"
+	"strconv"
+
+	"gopkg.in/go-playground/colors.v1"
 
 	"github.com/disintegration/imaging"
 )
@@ -34,6 +38,20 @@ func LeafedWumpus(BaseImageURL string, SleepingLeaf bool, UserWumpus Wumpus) (Wu
 		leafImage, _, err = image.Decode(leafURL.Body)
 		if err != nil {
 			fmt.Println(err)
+		}
+
+		pc, _ := colors.Parse("#" + strconv.FormatInt(int64(UserWumpus.Color), 16))
+		WumpusColors := pc.ToRGBA()
+		WumpusColor := color.NRGBA{
+			R: WumpusColors.R,
+			G: WumpusColors.G,
+			B: WumpusColors.B,
+			A: 255,
+		}
+		for x := 618; x < 821; x++ {
+			for y := 168; y < 323; y++ {
+				leafImage.(*image.NRGBA).SetNRGBA(x, y, WumpusColor)
+			}
 		}
 	} else {
 		leafURL, err := http.Get("https://orangeflare.me/imagehosting/Wumpagotchi/AsleepLeaf.png")

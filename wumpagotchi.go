79833c587b770c39eed481e554802439cf43c358
Josef Baltz <josef.baltz@proton.me>
2023-11-15 12:56:54 -0500

    Firestore via Base64 credentials

--- wumpagotchi.go
+++ wumpagotchi.go
@@ -1,14 +1,17 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"encoding/base64"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
 
+	"cloud.google.com/go/firestore"
 	"github.com/bwmarrin/discordgo"
 	"github.com/joho/godotenv"
+	"google.golang.org/api/option"
 )
 
 // MAIN
@@ -20,28 +23,33 @@ func main() {
 	}
 
 	// Build session
-	session, err := discordgo.New("Bot " + os.Getenv("DISCORD_TOKEN"))
+	ses, err := discordgo.New("Bot " + os.Getenv("DISCORD_TOKEN"))
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// Build Firestore client
+	ctx := context.Background()
+	d, _ := base64.StdEncoding.DecodeString(os.Getenv("GPC_CREDS_BASE64"))
+	fs, err := firestore.NewClient(ctx, os.Getenv("GCP_PROJECT_ID"), option.WithCredentialsJSON(d))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fs.Close()
 
 	// TODO: Add handlers to session
 
 	// Create and open websocket for Discord API using created session
-	err = session.Open()
+	err = ses.Open()
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("[Wumpagotchi] Online")
+	defer ses.Close()
 
 	// Wait for termination signal (Ctrl+C, etc)
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 	<-sc
-
-	// Close websocket for Discord API
-	session.Close()
-	fmt.Println("[Wumpagotchi] Offline")
 }
 
 // -- HANDLER FUNCTIONS
